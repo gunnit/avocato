f5cdafdc4a94ae85d007ea47ce7186ae0686b4b9
Greg <maric.gregor@gmal.com>
2024-11-21 22:01:27 +0100

    new settings prod

--- legal_assistant/settings/production.py
+++ legal_assistant/settings/production.py
@@ -1,53 +1,72 @@
 from .base import *
 
 # Security settings
-DEBUG = False
+DEBUG = env.bool('DJANGO_DEBUG', default=False)
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 
+# Azure Web App default domain and your custom domain
 ALLOWED_HOSTS = [
+    'avocato.azurewebsites.net',  # Default Azure domain
     'avocato-fvhmgsdxgtcbdxhz.germanywestcentral-01.azurewebsites.net',
     '.azurewebsites.net',
+    '*',  # Temporarily allow all hosts for troubleshooting
 ]
 
 # Database configuration for Azure PostgreSQL
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
+        'NAME': env('DB_NAME', default='avocato'),
+        'USER': env('DB_USER', default='postgres'),
         'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST'),
+        'HOST': env('DB_HOST', default='projectschool.postgres.database.azure.com'),
         'PORT': env('DB_PORT', default='5432'),
         'OPTIONS': {'sslmode': 'require'},
     }
 }
 
+# Middleware - Add WhiteNoise for static files
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Add WhiteNoise
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
 # Azure Blob Storage configuration
 DEFAULT_FILE_STORAGE = 'storages.backends.azure_storage.AzureStorage'
-AZURE_ACCOUNT_NAME = env('AZURE_ACCOUNT_NAME')
+AZURE_ACCOUNT_NAME = env('AZURE_ACCOUNT_NAME', default='greg123')
 AZURE_ACCOUNT_KEY = env('AZURE_ACCOUNT_KEY')
 AZURE_CONTAINER = env('AZURE_CONTAINER', default='media')
 AZURE_CUSTOM_DOMAIN = f'{AZURE_ACCOUNT_NAME}.blob.core.windows.net'
 AZURE_LOCATION = env('AZURE_LOCATION', default='francecentral')
 
-# Static files
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# Static files configuration
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'assets'),
+]
 
-# Security settings
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+# Security settings - Some relaxed for troubleshooting
+SECURE_SSL_REDIRECT = env.bool('DJANGO_SECURE_SSL_REDIRECT', default=True)
+SESSION_COOKIE_SECURE = env.bool('DJANGO_SESSION_COOKIE_SECURE', default=True)
+CSRF_COOKIE_SECURE = env.bool('DJANGO_CSRF_COOKIE_SECURE', default=True)
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_HSTS_SECONDS = 31536000
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_HSTS_PRELOAD = True
+SECURE_HSTS_SECONDS = env.int('DJANGO_SECURE_HSTS_SECONDS', default=31536000)
+SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool('DJANGO_SECURE_HSTS_SUBDOMAINS', default=True)
+SECURE_HSTS_PRELOAD = env.bool('DJANGO_SECURE_HSTS_PRELOAD', default=True)
 
 # CORS settings
 CORS_ALLOWED_ORIGINS = [
-    f"https://{host}" for host in ALLOWED_HOSTS
+    f"https://{host}" for host in ALLOWED_HOSTS if host != '*'
 ]
 CORS_ALLOW_CREDENTIALS = True
 
@@ -59,14 +78,18 @@ LOGGING = {
         'console': {
             'class': 'logging.StreamHandler',
         },
+        'file': {
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'django.log'),
+        },
     },
     'root': {
-        'handlers': ['console'],
+        'handlers': ['console', 'file'],
         'level': 'INFO',
     },
     'loggers': {
         'django': {
-            'handlers': ['console'],
+            'handlers': ['console', 'file'],
             'level': env('DJANGO_LOG_LEVEL', default='INFO'),
             'propagate': False,
         },
@@ -76,4 +99,4 @@ LOGGING = {
 # API Keys - Must be set in environment variables
 ANTHROPIC_API_KEY = env('ANTHROPIC_API_KEY')
 OPENAI_API_KEY = env('OPENAI_API_KEY')
-SERPER_API_KEY = env('SERPER_API_KEY')
+SERPER_API_KEY = env('SERPER_API_KEY', default='')
